58f8cd4813d54c98bc2ded50533887e91ed211e0
andrey-pavlov-13 <59314094+andrey-pavlov-13@users.noreply.github.com>
2020-09-21 13:41:31 +0300

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,15 +1,13 @@
-var s__selector = snake__body;
-var columns__in__table = 10;
-var rows__in__table = 10;
-
-function crate__table(columns__in__table, rows__in__table) { 
-    document.write('<table border="1">');
-    for (var i = 0; i < rows__in__table; i++) {
+function create__table(columns__in__table, rows__in__table) { 
+    document.write('<table border=1>');
+    for (let i = 0; i < rows__in__table; i++) {
         document.write('<tr>');
-        for (var j = 0; j < columns__in__table; j++) {
+        for (let j = 0; j < columns__in__table; j++) {
             document.write('<td></td>');
         }
         document.write('</tr>');
     }
     document.write('</table>');
 }
+
+create__table(10,10);
