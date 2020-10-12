0c8fda4cb9d18fcec273afa6be5f45e292e673fa
andrey-pavlov-13 <59314094+andrey-pavlov-13@users.noreply.github.com>
2020-10-12 22:03:18 +0300

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,14 +1,17 @@
-let s_table = document.querySelector('#s_table');
-function create__table(parent, columns, rows) { 
-    let table = document.createElement('table');
-    for (let i = 0; i < rows; i++) {
-        let tr = document.createElement('tr');
-        for (let j = 0; j < columns; j++) {
-            let td = document.createElement('td');
-            tr.appendChild(td);
-        }    
-    table.appendChild(tr);
-    }
-parent.appendChild(table);      
+
+button_table.onclick = function() {   
+    let s_table = document.querySelector('#s_table');
+    function create__table(parent, columns, rows) { 
+        let table = document.createElement('table');
+        for (let i = 0; i < rows; i++) {
+            let tr = document.createElement('tr');
+            for (let j = 0; j < columns; j++) {
+                let td = document.createElement('td');
+                tr.appendChild(td);
+            }    
+        table.appendChild(tr);
+        }
+    parent.appendChild(table);        
+} 
+create__table(s_table, 10,10);       
 }
-create__table(s_table, 10,10);  
