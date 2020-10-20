1bfe71069cf0f53e9ecbf1eb7e3b71e708e86376
nikkos <mykyta.kosukhin@sigma.software>
2020-10-20 12:38:38 +0300

    Апдейты для переменных, инициализация по завершению загрузки, фиксы для ивент хэндлера

--- snake.js
+++ snake.js
@@ -1,29 +1,37 @@
+'use strict';
 
-button_table.onclick = function() {
-    document.getElementById("s_table").innerHTML = "";   
-    let s_table = document.querySelector('#s_table');
-    function create__table(parent, columns, rows) { 
-        let table = document.createElement('table');
-        for (let i = 0; i < rows; i++) {
-            let tr = document.createElement('tr');
-            for (let j = 0; j < columns; j++) {
-                let td = document.createElement('td');
-                tr.appendChild(td);
-            }    
-        table.appendChild(tr);
+function create_table(parent, columns, rows) {
+    let table = document.createElement('table');
+    for (let i = 0; i < rows; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < columns; j++) {
+            let td = document.createElement('td');
+            tr.appendChild(td);
         }
-    parent.appendChild(table);        
-} 
-create__table(s_table, 10,10);       
+        table.appendChild(tr);
+    }
+    parent.appendChild(table);
 }
 
-table_cleaner.onclick = function cleaner(elementID){
-    document.getElementById("s_table").innerHTML = "";
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const button_table = document.getElementById('button_table');
+    const table_cleaner = document.getElementById('table_cleaner');
+    const click = document.getElementById('click');
+    const s_table = document.getElementById('s_table');
 
+    button_table.onclick = function() {
+        s_table.innerHTML = "";
+        create_table(s_table, 10,10);
+    }
 
-let rows_value = document.getElementById("table_rows").value;
-let colomns_value = document.getElementById("table_colomns").value;
-click.onclick = function input_cheker(rows_value, colomns_value) {
-    alert(rows_value);
-}
+    table_cleaner.onclick = function cleaner(elementID){
+        s_table.innerHTML = "";
+    }
+
+    click.onclick = function (event) {
+        let rows_value = document.getElementById("table_rows").value;
+        let columns_value = document.getElementById("table_columns").value;
+        console.log(rows_value, columns_value);
+        // alert(rows_value);
+    }
+})
